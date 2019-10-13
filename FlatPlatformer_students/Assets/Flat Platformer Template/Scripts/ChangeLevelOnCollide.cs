e95da990412ef54c62eaf483226ec79b54135f07
kathleenhayes <kathleen.hayes@digipen.edu>
2019-10-13 14:06:28 -0700

    student bloat file being added
    I hope this works

---
+++ FlatPlatformer_students/Assets/Flat Platformer Template/Scripts/ChangeLevelOnCollide.cs
@@ -0,0 +1,38 @@
+﻿/* By: Ryan Scheppler
+* Date: 10/9/19
+* Description: Add to object to cause a level change when the player touches it.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevelOnCollide : MonoBehaviour
+{
+    public string NextScene = "EndScene";
+    public AudioClip sfxDoor;
+
+    private AudioSource audioSource;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            audioSource.clip = sfxDoor;
+            audioSource.Play();
+            SceneManager.LoadScene(NextScene);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FlatPlatformer_students/Assets/Flat Platformer Template/Scripts/Health.cs
@@ -0,0 +1,61 @@
+﻿/* By: Ryan Scheppler
+ * Date: 10/9/19
+ * Description: Add to objects meant to have a health.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int CurrentHealth = 100;
+
+    public bool DestroyAtZero = true;
+
+    public UnityEvent DamageFunctions;
+    public UnityEvent DeathFunctions;
+    public UnityEvent HealFunctions;
+    private bool DeathOccured = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    //call this to deal damage
+    public void Damage( int damage)
+    {
+        CurrentHealth -= damage;
+        if (CurrentHealth <= 0 && !DeathOccured)
+        {
+            CurrentHealth = 0;
+            DeathFunctions.Invoke();
+            DeathOccured = true;
+            if (DestroyAtZero)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            DamageFunctions.Invoke();
+        }
+    }
+    //call this to heal
+    public void Heal(int health)
+    {
+        CurrentHealth += health;
+        if(CurrentHealth > MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+        }
+        HealFunctions.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
